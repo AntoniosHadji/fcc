c6dc8f913e94d7b6ebd262d13554fb49bc591ef0
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-10-14 13:37:15 -0400

    final quote machine

--- quotemachine/js/script.js
+++ quotemachine/js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-  // there is only one button on this page $("button")
+  // there is only one button on this page $('button')
   // if there were more, I could identify with id using #id
   // or class by using button.class
     getQuote();
 
-    $("#newquote").click(function(){
+    $('#newquote').click(function(){
       getQuote();
     });
+
 });
 
 function getQuote() {
@@ -18,17 +19,43 @@ function getQuote() {
           success: displayResponse,
           error: function(err) { alert(err); },
           beforeSend: function(xhr) {
-            xhr.setRequestHeader("X-Mashape-Key", "Ed9vPJR86VmshfeGiPlZvYDF3n6hp1g2a4Ejsnf6nhLVVS5h5v");
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.setRequestHeader("Accept", "application/json");
+            xhr.setRequestHeader('X-Mashape-Key', 'Ed9vPJR86VmshfeGiPlZvYDF3n6hp1g2a4Ejsnf6nhLVVS5h5v');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.setRequestHeader('Accept', 'application/json');
           }
       });
 }
 
 function displayResponse(data) {
-  if (data.hasOwnProperty("quote"))
-    $( "#quote" ).html(data.quote);
+    $( '#quote' ).html(data.quote);
+    $( '#author' ).html(data.author);
 
-  if (data.hasOwnProperty("author"))
-    $( "#author" ).html(data.author);
+  // these are various ways to search for elements
+  // both of these worked
+  // parent > child
+//  $('#tweet > iframe').each( function() {
+//    console.log('found: '+this.id);
+//  });
+  //
+//  element.class
+//  $('iframe.twitter-share-button').each( function() {
+//    console.log('found widget: '+this.id);
+//    $( this ).remove();
+//  });
+
+  // in the end, all I needed was to empty the tweet div since it was
+  // constructed as an empty div and the code below creates a new button
+  $('#tweet').empty();
+
+  twttr.widgets.createShareButton(
+    document.URL,
+    document.getElementById('tweet'),
+    {
+      size: 'large',
+      text: ''.concat(data.quote, ' ', data.author)
+    }
+  ).then( function( el ) {
+    console.log('Tweet button added: ' + el.id);
+  });
+
 }
