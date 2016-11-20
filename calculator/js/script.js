18a34af183a7abd7935e9bc5e704c0108638f700
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-11-19 20:47:38 -0500

    working calculator

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,57 +1,95 @@
-function calculate(exp) {
-  var result = 0;
-  result = exp.split(/[-+*\/=]/);
+// Found feature in Firfox 51,
+// return result; if result is not defined in function,
+// div with id="result" is returned
 
-  return result;
+/**
+ * this function calculates the entered expression
+ * @param {String} exp - expression
+ * @return {Number} evaluated expression
+ * **/
+function calculateExpression(exp) {
+  let numbers = exp.split(/[-+*\/]/);
+  let ops = exp.split(/[0-9\.]+/);
+  // remove first element === ''
+  ops.shift();
+  //remove last element === ''
+  ops.pop();
+
+  doOperation('*', numbers, ops);
+  doOperation('/', numbers, ops);
+  doOperation('+', numbers, ops);
+  doOperation('-', numbers, ops);
+
+  return numbers[0];
 }
 
-function parseExpression(exp) {
-  var arr = [];
-  var operators = ['-', '+', '*', '/', '=', '.'];
+function doOperation(op, num, ops) {
+  let i = ops.indexOf(op);
+  while (i !== -1) {
 
-  for (i=0; i<exp.length; i++) {
-    var item = exp.charAt(i);
-    if (operators.indexOf(item) === -1) {
-      if (i>0 && typeof arr[i-1] === 'number') {
-        arr[i-1] = arr[i-1]*10 + parseInt(item);
-      } else {
-        arr.push(parseInt(item));
-      }
-    } else {
-      arr.push(item);
-    }
+    let values = num.splice(i,2);
+    let calc = c(op);
+
+    num.splice(i, 0, calc(values[0], values[1]));
+    ops.splice(i, 1);
+    i = ops.indexOf(op);
   }
-  return arr;
 }
 
+function c(op) {
+  switch (op) {
+    case '/':
+      return function(a,b) { return a/b; };
+      // break not required because return passes control out of the function
+    case '*':
+      return function(a,b) { return a*b; };
+    case '+':
+      return function(a,b) { return 1*a+1*b; };
+    case '-':
+      return function(a,b) { return a-b; };
+  }
+}
 
 // extract value from button pressed and take appropriate action
-function processKeyPress() {
-  // log value of key
-  console.log( $(this).text());
-
-  if ( $( '#result' ).text().trim() === '0.00' ){
+function processNumKeyPress(event) {
+  isLastKeyPressOps = false;
+  if ( $( '#result' ).text().trim() === '' ){
     $( '#result' ).html( $( this ).text() );
   } else {
     $( '#result' ).append( $( this ).text() );
   }
-  // log current value of result as text
-  console.log( 'text: ' + $('#result').text().trim() );
 }
 
+// this starts as undefined which is coerces to false
+// undefined is falsy
+let isLastKeyPressOps;
+function processOpsKeyPress(event) {
+  if ( $( this ).text() === 'C' ) {
+    $( '#result' ).html( '' );
+  } else {
+    if (!isLastKeyPressOps) {
+      $( '#result' ).append( $( this ).text() );
+    }
+  }
+  isLastKeyPressOps = true;
+}
+//
+function processEqualsKeyPress(event) {
+  let exp = $( '#result' ).text().trim();
+  $( '#result' ).html( calculateExpression(exp).toString() );
+}
 
 function onReady() {
-  $( 'a' ).click( processKeyPress );
+  $( 'a.num' ).click( processNumKeyPress );
+  $( 'a.ops' ).click( processOpsKeyPress );
+  $( 'a.equalkey' ).click( processEqualsKeyPress );
 }
 
 $( document ).ready( onReady );
-
-
 
 
 // http://learn.jquery.com/using-jquery-core/avoid-conflicts-other-libraries/#use-the-argument-that-39-s-passed-to-the-jquery-document-ready-function
 // $ is alias for jQuery unless it has been taken over by another library
 // loaded after jQuery
 // more concise setup of the above line
-//$(function($) {
-
+//$(function($) { 
--- calculator/js/test.js
+++ calculator/js/test.js
@@ -1,24 +1,29 @@
-QUnit.test( "calculator functions", function( assert ) {
-  assert.equal(typeof calculate, 'function');
-  assert.equal(typeof parseExpression, 'function');
+//QUnit.test( "calculator parseExpression", function( assert ) {
+//  assert.equal(typeof parseExpression, 'function');
+//
+//  var test = [1, '+', 2, 3];
+//  assert.deepEqual( test, [1,'+',2,3]);
+//  assert.deepEqual(
+//    parseExpression('1+2*3-4/5'),
+//    [1, '+', 2, '*', 3, '-', 4, '/', 5]);
+//  assert.deepEqual(
+//    parseExpression('1+25*3-47/5'),
+//    [1, '+', 25, '*', 3, '-', 47, '/', 5]);
+//  // the 25 is working and 47 is not??
+//  // I have made progress in test driven development 
+//  // and also furthered my understanding of the qunit test framework
+//  assert.deepEqual(
+//    parseExpression('1+2.5*3-4.7/5'),
+//    [1, '+', 2.5, '*', 3, '-', 4.7, '/', 5]);
+//
+//
+//});
 
-  var test = [1, '+', 2, 3];
-  assert.deepEqual( test, [1,'+',2,3]);
-  assert.deepEqual(
-    parseExpression('1+2*3-4/5'),
-    [1, '+', 2, '*', 3, '-', 4, '/', 5]);
-  assert.deepEqual(
-    parseExpression('1+25*3-47/5'),
-    [1, '+', 25, '*', 3, '-', 47, '/', 5]);
-  // the 25 is working and 47 is not??
-  // I have made progress in test driven development 
-  // and also furthered my understanding of the qunit test framework
-  assert.deepEqual(
-    parseExpression('1+2.5*3-4.7/5'),
-    [1, '+', 2.5, '*', 3, '-', 4.7, '/', 5]);
-  assert.equal( calculate('1+2*3-4/5'), 6.2);
-  assert.equal( calculate('1+2*3*4/5'), 5.8);
-  assert.equal( calculate('1+25*3-47/5'), 66.6);
-  assert.equal( calculate('1+2.5*3-4.7/5'), 7.56);
-
+QUnit.test( "calculator calculate", function( assert ) {
+  assert.equal(typeof calculateExpression, 'function');
+  assert.equal( calculateExpression('2*3'), 6);
+  assert.equal( calculateExpression('1+2*3-4/5'), 6.2);
+  assert.equal( calculateExpression('1+2*3*4/5'), 5.8);
+  assert.equal( calculateExpression('1+25*3-47/5'), 66.6);
+  assert.equal( calculateExpression('1+2.5*3-4.7/5'), 7.56);
 });
