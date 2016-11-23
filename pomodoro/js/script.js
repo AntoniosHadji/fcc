ffbb5e555903eaba55c0a360c94084cb31050f07
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-11-22 19:07:56 -0500

    values not showing up? unexplained error

--- pomodoro/js/script.js
+++ pomodoro/js/script.js
@@ -1,32 +1,63 @@
 // Pomodoro clock
 // Questions: How to used timers?
 
-
-function getTimeRemaining(endtime){
+function getTimeRemaining(endtime) {
   let t = Date.parse(endtime) - Date.parse(new Date());
-  let seconds = Math.floor( (t/1000) % 60 );
-  let minutes = Math.floor( (t/1000/60) % 60 );
-  let hours = Math.floor( (t/(1000*60*60)) % 24 );
-  let days = Math.floor( t/(1000*60*60*24) );
+  let seconds = Math.floor((t / 1000) % 60);
+  let minutes = Math.floor((t / 1000 / 60) % 60);
+  let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  let days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds,
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
   };
 }
+
+function initializeClock(id, endtime) {
+  let clock = document.getElementById(id);
+  let timeinterval = setInterval(function() {
+    let t = getTimeRemaining(endtime);
+    clock.innerHTML =  t.minutes + ':' +  t.seconds;
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+
 
 /**
  *
  */
 function onReady() {
   console.log('onReady');
-  let stopTime = moment().add(25, 'minutes');
-  $( '#pomodoro' ).html( 'time: ' + stopTime.format() + '</br>' );
+  let timer = document.getElementById('pomodoro')
+    , now = new Date()
+    , deadline = new Date(now.getFullYear, now.getMonth, now.getDate,
+      now.getHours, now.getMinutes + 25);
 
-};
+  timer.innerHTML = countdown(deadline).toString();
+  setInterval(function(){
+    timer.innerHTML = countdown(deadline ).toString();
+  }, 1000);
 
-$( document ).ready( onReady );
+  //let stopTime = new Date(0,0,0,0,25);
+  //let currentTime = stopTime;
+  //let startTime = new Date();
 
+  //let p = document.getElementById('pomodoro');
+  //p.innerHTML = currentTime.toLocaleFormat('%M:%S');
+  //console.log(currentTime.toLocaleFormat('%M:%S'));
+  //$('#pomodoro').html = currentTime.toLocaleFormat('%M:%S');
+  console.log('onReady Completed');
+}
 
+window.onload = onReady();
+//$(document).ready( onReady );
+// http://www.w3schools.com/js/tryit.asp?filename=tryjs_setinterval2
+// http://www.w3schools.com/js/js_timing.asp
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setInterval
+//
