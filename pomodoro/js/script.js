0361c68cea9a10486d6cfd6e78ae912ff5604992
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-11-23 16:34:57 -0500

    completed user stories

--- pomodoro/js/script.js
+++ pomodoro/js/script.js
@@ -1,63 +1,86 @@
 // Pomodoro clock
 // Questions: How to used timers?
+// Use pure javascript and DOM api
+// depends on countdown.js http://countdownjs.org/readme.html
 
-function getTimeRemaining(endtime) {
-  let t = Date.parse(endtime) - Date.parse(new Date());
-  let seconds = Math.floor((t / 1000) % 60);
-  let minutes = Math.floor((t / 1000 / 60) % 60);
-  let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  let days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    total: t,
-    days: days,
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds
-  };
-}
-
-function initializeClock(id, endtime) {
-  let clock = document.getElementById(id);
-  let timeinterval = setInterval(function() {
-    let t = getTimeRemaining(endtime);
-    clock.innerHTML =  t.minutes + ':' +  t.seconds;
-    if(t.total<=0){
-      clearInterval(timeinterval);
-    }
-  },1000);
-}
-
-
+//global variables
+window.timerId;
 
 /**
- *
+ * no dependency
+ * return endtime of session
  */
+function returnSessionTargetTime(sessionTime) {
+  let now = new Date();
+  let target = new Date(
+    now.getFullYear(), now.getMonth(), now.getDate(),
+    now.getHours(), now.getMinutes() + sessionTime, now.getSeconds()
+  );
+  return target;
+}
+
+
+document.getElementById('resetTimer').addEventListener('click',
+  function(event) {
+    console.log('click: ' + event.target.id);
+    window.clearInterval(window.timerId);
+    window.timerId = null;
+    document.getElementById('pomodoro').innerHTML = null;
+    let report = window.pomodoroStartTime.toString();
+    report += ' ' + window.pomodoroStopTime.toString();
+    addData(report);
+  }
+);
+
+document.getElementById('startTimer').addEventListener('click',
+  function(event) {
+    console.log('click: ' + event.target.id);
+    window.pomodoroStartTime = new Date();
+    if (window.timerId) { window.clearInterval(window.timerId) };
+    let pval = parseInt(document.getElementById('ptime').value);
+    let bval = parseInt(document.getElementById('btime').value);
+
+    window.timerId =
+      countdown(
+        returnSessionTargetTime(pval),
+        function(ts) {
+          if (ts.value < 0) {
+            document.getElementById('pomodoro').innerHTML = ts
+              .toHTML('strong', 'Break Time!!')
+              .replace('and', '</br>and</br>');
+          } else {
+            if (ts.value < bval*60*1000 - 1000) {
+              document.getElementById('pomodoro').innerHTML = 'Break Time:</br>';
+              document.getElementById('pomodoro').innerHTML += ts
+                .toHTML('strong', 'Completed!!')
+                .replace('and', '</br>and</br>');
+            } else {
+              window.pomodoroStopTime = new Date();
+              document.getElementById('pomodoro').innerHTML = 'Completed!!';
+              document.clearInterval(window.timerId);
+            }
+          }
+          document.getElementById('pomodoro').innerHTML
+        },
+        countdown.DEFAULTS, NaN, 0 // default values for countdown.js
+      );
+  }
+);
+
 function onReady() {
-  console.log('onReady');
-  let timer = document.getElementById('pomodoro')
-    , now = new Date()
-    , deadline = new Date(now.getFullYear, now.getMonth, now.getDate,
-      now.getHours, now.getMinutes + 25);
+  document.getElementById("startTimer").className =
+   document.getElementById("startTimer").className.replace
+      ( /(?:^|\s)disabled(?!\S)/g , '' )
 
-  timer.innerHTML = countdown(deadline).toString();
-  setInterval(function(){
-    timer.innerHTML = countdown(deadline ).toString();
-  }, 1000);
+  document.getElementById("resetTimer").className =
+   document.getElementById("resetTimer").className.replace
+      ( /(?:^|\s)disabled(?!\S)/g , '' )
 
-  //let stopTime = new Date(0,0,0,0,25);
-  //let currentTime = stopTime;
-  //let startTime = new Date();
-
-  //let p = document.getElementById('pomodoro');
-  //p.innerHTML = currentTime.toLocaleFormat('%M:%S');
-  //console.log(currentTime.toLocaleFormat('%M:%S'));
-  //$('#pomodoro').html = currentTime.toLocaleFormat('%M:%S');
   console.log('onReady Completed');
 }
 
 window.onload = onReady();
-//$(document).ready( onReady );
+
 // http://www.w3schools.com/js/tryit.asp?filename=tryjs_setinterval2
 // http://www.w3schools.com/js/js_timing.asp
 // https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setInterval
-//
