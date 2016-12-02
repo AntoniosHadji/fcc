5d8c64d83ecd67138703fd7e41d1fa15d2cc3126
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-12-02 14:51:37 -0500

    ex7 complete

--- nodeschool.io/learnyounode/ex7-http-client.js
+++ nodeschool.io/learnyounode/ex7-http-client.js
@@ -18,24 +18,10 @@ http.get(process.argv[2], cbResponseHandler)
  * @param {Object} response response object
  */
 function cbResponseHandler(response) {
-  const statusCode = response.statusCode;
-
-  // handle error condition first
-  let error;
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.\n` +
-                      `Status Code: ${statusCode}`);
-  }
-  if (error) {
-    console.error(error.message);
-    // consume response data to free up memory
-    response.resume();
-    return;
-  }
-
   // then process response
   response.setEncoding('utf8');
-  response.on('data', (chunk) => console.log(chunk));
+  response.on('data', console.log);
+  response.on('error', console.error);
 }
 
 /** handle errors
