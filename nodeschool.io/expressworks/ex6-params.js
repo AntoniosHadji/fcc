edb7e103be47fa66bedac2c38635665fb684a36f
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-12-09 18:56:37 -0500

    typo

--- nodeschool.io/expressworks/ex6-params.js
+++ nodeschool.io/expressworks/ex6-params.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+// app.param('id', (req, res, next, messageId) => {
+//   req.computedHash = require('crypto')
+//                       .createHash('sha1')
+//                       .update(new Date().toDateString() + messageId)
+//                       .digest('hex');
+//   next();
+// });
+//
+// app.get('message/:id', (req, res, next) => {
+//   res.send(req.computedHash);
+// });
+
+
 app.put('/message/:id', (req, res) => {
-  let id = req.param.id;
+  let id = req.params.id;
   let str = require('crypto')
               .createHash('sha1')
               .update(new Date().toDateString() + id)
