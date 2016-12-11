bc681f2b5eebddc5da818b3510ece54076e946ca
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2016-12-11 18:39:55 -0500

    ex8 complete

---
+++ nodeschool.io/expressworks/ex8-json.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.get('/books', (req, res) => {
+  fs.readFile(process.argv[3], (e, data) => {
+    if (e) return res.sendStatus(500);
+   try {
+    books = JSON.parse(data);
+   } catch (e) {
+     res.sendStatus(500);
+   }
+    res.json(books);
+  });
+});
+app.listen(process.argv[2]);
+
+
