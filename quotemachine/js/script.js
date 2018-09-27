8ff567d4bca1a6e406abf3b6cb929af1cf0b821f
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-09-27 10:44:28 -0400

    add error message output

--- quotemachine/js/script.js
+++ quotemachine/js/script.js
@@ -1,51 +1,51 @@
-$(document).ready(function(){
+/* global $ twttr */
+$(document).ready(function () {
   // there is only one button on this page $('button')
   // if there were more, I could identify with id using #id
   // or class by using button.class
-    getQuote();
+  getQuote()
 
-    $('#newquote').click(function(){
-      getQuote();
-    });
+  $('#newquote').click(function () {
+    getQuote()
+  })
+})
 
-});
-
-function getQuote() {
-      $.ajax({
-          url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
-          type: 'POST', // The HTTP Method, can be GET POST PUT DELETE etc
-          data: {}, // Additional parameters here
-          dataType: 'json',
-          success: displayResponse,
-          error: function(err) { alert(err); },
-          beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Mashape-Key', 'Ed9vPJR86VmshfeGiPlZvYDF3n6hp1g2a4Ejsnf6nhLVVS5h5v');
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.setRequestHeader('Accept', 'application/json');
-          }
-      });
+function getQuote () {
+  $.ajax({
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+    type: 'POST', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+    dataType: 'json',
+    success: displayResponse,
+    error: function (err) { console.error(err) },
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader('X-Mashape-Key', 'Ed9vPJR86VmshfeGiPlZvYDF3n6hp1g2a4Ejsnf6nhLVVS5h5v')
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+      xhr.setRequestHeader('Accept', 'application/json')
+    }
+  })
 }
 
-function displayResponse(data) {
-    $( '#quote' ).html(data.quote);
-    $( '#author' ).html(data.author);
+function displayResponse (data) {
+  $('#quote').html(data.quote)
+  $('#author').html(data.author)
 
   // these are various ways to search for elements
   // both of these worked
   // parent > child
-//  $('#tweet > iframe').each( function() {
-//    console.log('found: '+this.id);
-//  });
+  //  $('#tweet > iframe').each( function() {
+  //    console.log('found: '+this.id);
+  //  });
   //
-//  element.class
-//  $('iframe.twitter-share-button').each( function() {
-//    console.log('found widget: '+this.id);
-//    $( this ).remove();
-//  });
+  //  element.class
+  //  $('iframe.twitter-share-button').each( function() {
+  //    console.log('found widget: '+this.id);
+  //    $( this ).remove();
+  //  });
 
   // in the end, all I needed was to empty the tweet div since it was
   // constructed as an empty div and the code below creates a new button
-  $('#tweet').empty();
+  $('#tweet').empty()
 
   twttr.widgets.createShareButton(
     document.URL,
@@ -54,8 +54,7 @@ function displayResponse(data) {
       size: 'large',
       text: ''.concat(data.quote, ' ', data.author)
     }
-  ).then( function( el ) {
-    console.log('Tweet button added: ' + el.id);
-  });
-
+  ).then(function (el) {
+    console.log('Tweet button added: ' + el.id)
+  })
 }
