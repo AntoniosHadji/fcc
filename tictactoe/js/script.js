3251a66d34af633f220541a2ed7d95ee8aafbe4a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-09-27 12:49:29 -0400

    fixed text overflow on large screen

--- tictactoe/js/script.js
+++ tictactoe/js/script.js
@@ -1,77 +1,78 @@
+/* global $ */
 const app = {
   player: 'X',
   computer: 'O',
 
   slot: {
-       'tl': 0,
-       'tm': 1,
-       'tr': 2,
-       'ml': 3,
-       'mm': 4,
-       'mr': 5,
-       'bl': 6,
-       'bm': 7,
-       'br': 8,
+    'tl': 0,
+    'tm': 1,
+    'tr': 2,
+    'ml': 3,
+    'mm': 4,
+    'mr': 5,
+    'bl': 6,
+    'bm': 7,
+    'br': 8
   },
 
   returnID: [
-       '#tl div.table-cell',
-       '#tm div.table-cell',
-       '#tr div.table-cell',
-       '#ml div.table-cell',
-       '#mm div.table-cell',
-       '#mr div.table-cell',
-       '#bl div.table-cell',
-       '#bm div.table-cell',
-       '#br div.table-cell',
+    '#tl div.table-cell',
+    '#tm div.table-cell',
+    '#tr div.table-cell',
+    '#ml div.table-cell',
+    '#mm div.table-cell',
+    '#mr div.table-cell',
+    '#bl div.table-cell',
+    '#bm div.table-cell',
+    '#br div.table-cell'
   ],
 
-  onReady: function() {
-    $('li').click( app.choosePlayer );
-    $('.squares').click( player.placeSquare );
-    $('#myModal button').click( app.reset );
-    console.log('ready');
-    console.log('Player is ' + app.player);
-    console.log('Computer is ' + app.computer);
+  onReady: function () {
+    $('li').click(app.choosePlayer)
+    $('.squares').click(player.placeSquare)
+    $('#myModal button').click(app.reset)
+    console.log('ready')
+    console.log('Player is ' + app.player)
+    console.log('Computer is ' + app.computer)
   },
 
-  choosePlayer: function(event) {
-    $( 'li#playx' ).toggleClass( 'active' );
-    $( 'li#playo' ).toggleClass( 'active' );
+  choosePlayer: function (event) {
+    $('li#playx').toggleClass('active')
+    $('li#playo').toggleClass('active')
 
     if (app.player === 'X') {
-      app.player = 'O';
-      app.computer = 'X';
+      app.player = 'O'
+      app.computer = 'X'
     } else {
-      app.player = 'X';
-      app.computer = 'O';
+      app.player = 'X'
+      app.computer = 'O'
     }
-    console.log('Player is ' + app.player);
-    console.log('Computer is ' + app.computer);
+    console.log('Player is ' + app.player)
+    console.log('Computer is ' + app.computer)
   },
 
-  gameOver: function(winner) {
+  gameOver: function (winner) {
     // The only possibility is that computer wins or there is a tie.
     if (winner === app.computer) {
-      $('#myModal div.modal-body').html('Computer Wins');
-      $('#myModal').modal('show');
+      $('#myModal div.modal-body').html('Computer Wins')
+      $('#myModal').modal('show')
     } else {
       $('#myModal div.modal-body')
-        .html('Game ends in a tie! You can not beat the computer');
-      $('#myModal').modal('show');
+        .html('Game ends in a tie! You can not beat the computer')
+      $('#myModal').modal('show')
     }
   },
 
-  reset: function() {
+  reset: function () {
     // reset html table-cells
-    $( '.table-cell' ).each( function() {
-        $(this).html('');
-    });
+    $('.table-cell').each(function () {
+      $(this).html('')
+    })
 
     // reset board
-    computer.board = new Array(9);
-  },
-};
+    computer.board = new Array(9)
+  }
+}
 
 const computer = {
   board: new Array(9),
@@ -85,109 +86,110 @@ const computer = {
     [1, 4, 7], // 4 middle column
     [2, 5, 8], // 5 right column
     [0, 4, 8], // 6 diagonals
-    [2, 4, 6], // 7
+    [2, 4, 6] // 7
   ],
 
-  returnOptions: function() {
+  returnOptions: function () {
     // hold possible moves
-    let result = [];
+    let result = []
 
     // iterate through the winning paths to calculate all possible moves
-    computer.winPaths.forEach(function(element, index, arr) {
+    computer.winPaths.forEach(function (element, index, arr) {
       // row[0]=computer, row[1]=player, row[2]=index, row[3]=potential play
-      let row = [];
-      let p = 0;
-      let c = 0;
+      let row = []
+      let p = 0
+      let c = 0
       // iterate through the winning paths and count filled squares
-      element.forEach(function(e, i, a) {
-        if (computer.board[e] === app.player) p++;
-        if (computer.board[e] === app.computer) c++;
+      element.forEach(function (e, i, a) {
+        if (computer.board[e] === app.player) p++
+        if (computer.board[e] === app.computer) c++
         if (computer.board[e] === undefined) { // && row[3] === undefined) {
-          row[3] = i;
+          row[3] = i
         }
-      });
+      })
 
-      row[0] = c;
-      row[1] = p;
-      row[2] = index; // index of the winning path
+      row[0] = c
+      row[1] = p
+      row[2] = index // index of the winning path
       // if the row is not filled, push it onto the result stack
-      if ( p + c !== 3 ) {
-        result.push(row);
+      if (p + c !== 3) {
+        result.push(row)
       }
-    });
+    })
 
-    result.sort();
-    return result;
+    result.sort()
+    return result
   },
 
-  returnSquareIndex: function(resultArray) {
-    let r;
-    resultArray.forEach( function(e, i, a) {
-        if (e[0] === 2) r = e;
-        if (e[1] === 2) r = e;
-    });
+  returnSquareIndex: function (resultArray) {
+    let r
+    resultArray.forEach(function (e, i, a) {
+      if (e[0] === 2) r = e
+      if (e[1] === 2) r = e
+    })
 
     if (r === undefined) {
       // fix for case when player plays ml and either tm or bm
       // without this code, player can set up a win
       if (computer.board[3] === app.player) {
-        if (computer.board[7] == app.player && computer.board[6] == undefined) {
-          return [0, 0, 2, 0];
+        if (computer.board[7] === app.player && computer.board[6] === undefined) {
+          return [0, 0, 2, 0]
         }
-        if (computer.board[1] == app.player && computer.board[0] == undefined) {
-          return [0, 0, 0, 0];
+        if (computer.board[1] === app.player && computer.board[0] === undefined) {
+          return [0, 0, 0, 0]
         }
       }
-      return resultArray[0];
+      return resultArray[0]
     } else {
-      return r;
+      return r
     }
   },
 
-  placeSquare: function() {
+  placeSquare: function () {
     // if middle square is not played play it
-    if ( computer.board[4] === undefined ) {
-      $( app.returnID[4] ).text(app.computer);
-      computer.board[4] = app.computer;
+    if (computer.board[4] === undefined) {
+      $(app.returnID[4]).text(app.computer)
+      computer.board[4] = app.computer
     } else {
       // select choice from rows computer has 2 slots first,
       // then player has two slots
       // then any non played slot
-      let data = computer.returnSquareIndex(computer.returnOptions());
-      if (data === undefined ) {
+      let data = computer.returnSquareIndex(computer.returnOptions())
+      if (data === undefined) {
         // no play options means that game ended in a tie
-        console.log('Game Complete: tie');
-        app.gameOver('tie');
+        console.log('Game Complete: tie')
+        app.gameOver('tie')
       } else {
         // select slot for computer
-        let square = computer.winPaths[data[2]][data[3]];
-        let id = app.returnID[square];
-        $( id ).text(app.computer);
+        let square = computer.winPaths[data[2]][data[3]]
+        let id = app.returnID[square]
+        $(id).text(app.computer)
         if (data[0] === 2 && data[2] <= 7) {
-          console.log('Game Complete: computer wins');
-          app.gameOver(app.computer);
+          console.log('Game Complete: computer wins')
+          app.gameOver(app.computer)
         } else {
-          computer.board[app.slot[id.substr(1, 2)]] = app.computer;
+          computer.board[app.slot[id.substr(1, 2)]] = app.computer
         }
       }
     }
-  },
+  }
 
-};
+}
 
 const player = {
-  placeSquare: function(event) {
+  placeSquare: function (event) {
     // whole event for inspection
-    $( event.target ).text( app.player );
+    $(event.target).text(app.player)
+    console.log(event.target)
     // square id
-    let square = app.slot[event.currentTarget.id];
-    computer.board[square] = app.player;
-    computer.placeSquare();
-  },
-};
+    let square = app.slot[event.currentTarget.id]
+    computer.board[square] = app.player
+    window.setTimeout(computer.placeSquare, 100)
+  }
+}
 
 // DOM API commmand - jQuery not needed for this functionality
-window.onload = app.onReady();
+window.onload = app.onReady()
 
 // NOTES:
 // this works to call javascript function from html
